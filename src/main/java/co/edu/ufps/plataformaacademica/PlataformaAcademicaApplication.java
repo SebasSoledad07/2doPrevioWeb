038a28fb350ea2c26fb7efc153e51950fa1d9c68
SebasSoledad07 <sebastiansg@ufps.edu.co>
2025-05-09 07:29:21 -0400

    proyecto creado

---
+++ src/main/java/co/edu/ufps/plataformaacademica/PlataformaAcademicaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.plataformaacademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaAcademicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaAcademicaApplication.class, args);
+    }
+
+}
